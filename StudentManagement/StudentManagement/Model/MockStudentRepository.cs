b4c2cebcf0671314dc8d14628ebbc33e3d794505
Unknown <unknown@example.com>
2021-03-08 09:43:42 +0800

    ASP.NET Core MVC 中的Model 模型

---
+++ StudentManagement/StudentManagement/Model/MockStudentRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Model
+{
+    public class MockStudentRepository : IStudentRepository
+    {
+        private List<Student> _studentList;
+
+        public MockStudentRepository()
+        {
+            _studentList = new List<Student>()
+            {
+            new Student() { Id = 1, Name = "张三", Major = "一年级", Email = "Tony-zhang@52abp.com" },
+            new Student() { Id = 2, Name = "李四", Major = "二年级", Email = "lisi@52abp.com" },
+            new Student() { Id = 3, Name = "王二麻子", Major = "二年级", Email = "wang@52abp.com" },
+            };
+        }
+
+
+        public Student GetStudent(int id)
+        {
+            return _studentList.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
---
+++ StudentManagement/StudentManagement/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Major { get; set; }
+
+        public string Email { get; set; }
+    }
+}
