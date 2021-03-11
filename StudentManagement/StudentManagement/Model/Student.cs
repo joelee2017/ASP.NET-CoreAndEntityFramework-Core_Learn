befa86915fc6a23b751af317fb19eb284982f521
Unknown <unknown@example.com>
2021-03-11 15:37:29 +0800

    ASP.NET Core 中select 選擇器驗證

--- StudentManagement/StudentManagement/Model/Student.cs
+++ StudentManagement/StudentManagement/Model/Student.cs
@@ -9,15 +9,17 @@ namespace StudentManagement.Model
         public int Id { get; set; }
 
         [Display(Name = "姓名")]
-        [Required, MaxLength(50, ErrorMessage = "名字的長度不能超過50个字")]
+        [Required(ErrorMessage ="請輸入名字"), MaxLength(50, ErrorMessage = "名字的長度不能超過50个字")]
         public string Name { get; set; }
+    
+        [Display(Name = "主修科目")]
+        [Required(ErrorMessage = "請選擇主修科目")]
+        public MajorEnum? Major { get; set; }
 
-        public MajorEnum Major { get; set; }
-
-        [Display(Name = "電子郵件")]
+        [Display(Name = "電子信箱")]
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
         ErrorMessage = "信箱的格式不正確")]
-        [Required]
+        [Required(ErrorMessage = "請輸入電子信箱")]
         public string Email { get; set; }
     }
 }
